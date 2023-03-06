3f8f47d9c570f34015e2947c68163090a05b6348
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-03-06 16:45:02 +0300

    set window ui as in UserDefaults

--- YoungTalent/Application/SceneDelegate.swift
+++ YoungTalent/Application/SceneDelegate.swift
@@ -17,6 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        guard let isDarkMode = UserDefaults.standard.object(forKey: "isDarkMode") as? Bool else{ return }
+        window?.overrideUserInterfaceStyle = isDarkMode ? .dark: .light
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
