bec120b2b99990c86d1f862ab5660598cec04d1a
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-03-13 18:17:07 +0300

    update ui

--- YoungTalent/Cells/CommunityGroupCell.swift
+++ YoungTalent/Cells/CommunityGroupCell.swift
@@ -17,10 +17,27 @@ class CommunityGroupCell: UICollectionViewCell {
     @IBOutlet weak var memberImageView2: UIImageView!
     @IBOutlet weak var memberImageView3: UIImageView!
     @IBOutlet weak var moreMemberLabel: UILabel!
-    
+    @IBOutlet weak var moreMemberView: UIView!
+    @IBOutlet weak var stackView: UIStackView!
     //MARK: - Lifecycle
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        setupView()
     }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        guard traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) else{ return }
+        memberImageView1.layer.borderColor = UIColor(named: "backgroundColor")!.cgColor
+        memberImageView2.layer.borderColor = UIColor(named: "backgroundColor")!.cgColor
+        memberImageView3.layer.borderColor = UIColor(named: "backgroundColor")!.cgColor
+    }
+    
+    func setupView(){
+        stackView.bringSubviewToFront(memberImageView3)
+        stackView.bringSubviewToFront(memberImageView2)
+        stackView.bringSubviewToFront(memberImageView1)
+    }
+    
 }
