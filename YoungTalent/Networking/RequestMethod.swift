e4586cb2f719db9bdc469e27ab406d81a2f9f47f
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-03-14 09:27:52 +0300

    add Networking

---
+++ YoungTalent/Networking/RequestMethod.swift
@@ -0,0 +1,14 @@
+//
+//  RequestMethod.swift
+//  YoungTalent
+//
+//  Created by admin on 10.03.2023.
+//
+
+public enum RequestMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
