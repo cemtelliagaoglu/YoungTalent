8c154e029b767807fcb3e48ba30225b5c46e82ff
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-03-14 18:30:02 +0300

    refactor: apply CleanSwift to Register scene

---
+++ YoungTalent/Scenes/Register/RegisterPresenter.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterPresenter.swift
+//  YoungTalent
+//
+//  Created by admin on 14.03.2023.
+//
+
+import Foundation
+
+protocol RegisterPresentationLogic: AnyObject {
+    
+}
+
+final class RegisterPresenter: RegisterPresentationLogic {
+    
+    weak var viewController: RegisterDisplayLogic?
+    
+}
---
+++ YoungTalent/Scenes/Register/RegisterRouter.swift
@@ -0,0 +1,34 @@
+//
+//  RegisterRouter.swift
+//  YoungTalent
+//
+//  Created by admin on 14.03.2023.
+//
+
+import UIKit
+
+protocol RegisterRoutingLogic: AnyObject {
+    func popVC()
+    func routeToHome()
+}
+
+protocol RegisterDataPassing: AnyObject {
+    var dataStore: RegisterDataStore? { get }
+}
+
+final class RegisterRouter: RegisterRoutingLogic, RegisterDataPassing {
+    
+    weak var viewController: RegisterVC?
+    var dataStore: RegisterDataStore?
+    
+    func popVC() {
+        viewController?.navigationController?.popViewController(animated: true)
+    }
+    
+    func routeToHome() {
+        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+        let destinationVC = storyboard.instantiateViewController(withIdentifier: "HomeVC")
+        viewController?.navigationController?.pushViewController(destinationVC, animated: true)
+    }
+    
+}
