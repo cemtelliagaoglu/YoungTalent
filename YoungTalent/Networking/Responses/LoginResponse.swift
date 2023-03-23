98a272d7c5f6d55250ad18471a2b07f2eb58b55e
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-03-23 19:18:23 +0300

    Update LoginResponse.swift

--- YoungTalent/Networking/Responses/LoginResponse.swift
+++ YoungTalent/Networking/Responses/LoginResponse.swift
@@ -7,16 +7,18 @@
 
 import Foundation
 
-public struct LoginResponse: Codable{
+public struct LoginResponse: Codable {
     public let error: Bool
     public let reason: String
     public let body: LoginBody?
-    
-    //MARK: - LoginBody
+
+    // MARK: - LoginBody
+
     public struct LoginBody: Codable {
         public let id: String
         public let nameSurname: String
         public let title: String
+
         public let profilePhoto: String?
         public let accessToken: String
         public let refreshToken: String
@@ -26,7 +28,9 @@ public struct LoginResponse: Codable{
         public let pushNotificationEnabled: Bool
         public let team: Team?
     }
+
     // MARK: - Team
+
     public struct Team: Codable {
         public let createdAt: String
         public let id: String
@@ -35,7 +39,9 @@ public struct LoginResponse: Codable{
         public let name: String
         public let company: Company?
     }
+
     // MARK: - Company
+
     public struct Company: Codable {
         public let id: String
     }
