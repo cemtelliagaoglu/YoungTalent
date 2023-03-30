1c386a94ebf2a8417c8e617bfad24333df84f7a6
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-03-30 16:24:06 +0300

    add UserProfile entity

---
+++ YoungTalent/CoreData/Entities/UserProfile+CoreDataClass.swift
@@ -0,0 +1,12 @@
+//
+//  UserProfile+CoreDataClass.swift
+//  YoungTalent
+//
+//  Created by admin on 28.03.2023.
+//
+//
+
+import CoreData
+import Foundation
+
+@objc(UserProfile) public class UserProfile: NSManagedObject {}
