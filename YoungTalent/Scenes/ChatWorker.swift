9b6273565c22eb49e5187b27c698132eea67179f
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-04-04 20:21:04 +0300

    Add getCurrentLocation method

--- YoungTalent/Scenes/ChatWorker.swift
+++ YoungTalent/Scenes/ChatWorker.swift
@@ -7,6 +7,26 @@
 
 import Foundation
 
-protocol ChatWorkingLogic: AnyObject {}
+protocol ChatWorkingLogic: AnyObject {
+    func getCurrentLocation(completion: @escaping ((Chat.Case.Location) -> Void))
+}
 
-final class ChatWorker: ChatWorkingLogic {}
+final class ChatWorker: ChatWorkingLogic {
+    private let mapManager = MapManager.shared
+    private let notificationCenter = NotificationCenter.default
+
+    func getCurrentLocation(completion: @escaping ((Chat.Case.Location) -> Void)) {
+        mapManager.requestCurrentLocation()
+        notificationCenter.addObserver(
+            forName: .sharedLocation,
+            object: nil,
+            queue: .main
+        ) { notification in
+            guard let location = notification.object as? [String: Any],
+                  let longitude = location["lon"] as? Double,
+                  let latitude = location["lat"] as? Double
+            else { return }
+            completion(.init(longitude: longitude, latitude: latitude))
+        }
+    }
+}
