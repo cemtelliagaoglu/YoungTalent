119232496ca1a33f4d75b99d94668950a13e5ea9
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-04-04 20:19:09 +0300

    Add fetchLocation method and location storage

--- YoungTalent/Scenes/ChatInteractor.swift
+++ YoungTalent/Scenes/ChatInteractor.swift
@@ -9,16 +9,19 @@ import Foundation
 
 protocol ChatBusinessLogic: AnyObject {
     func fetchData()
+    func fetchLocation()
 }
 
 protocol ChatDataStore: AnyObject {
     var contact: User? { get set }
+    var location: Chat.Case.Location? { get set }
 }
 
 final class ChatInteractor: ChatBusinessLogic, ChatDataStore {
     var presenter: ChatPresentationLogic?
     var worker: ChatWorkingLogic = ChatWorker()
     var contact: User?
+    var location: Chat.Case.Location?
 
     func fetchData() {
         // fetch contact
@@ -26,6 +29,12 @@ final class ChatInteractor: ChatBusinessLogic, ChatDataStore {
             presenter?.presentContact(.init(user: contact))
         } else {
             presenter?.presentErrorMessage("Failed to load contact info")
+        }
+    }
+
+    func fetchLocation() {
+        worker.getCurrentLocation { [weak self] location in
+            self?.location = location
         }
     }
 }
